27589e26a2a31b3b67c5f7c92d73a29923824843
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-02-11 10:15:36 -0300

    feat: enhance authentication flow with Swagger documentation and refactor signIn method to use SignInDto

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,4 +1,20 @@
-export interface AuthResponse {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AuthResponse {
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhNWJjOGE1NS1jZjdmLTQzYWYtODU0MC05MGI4YmM2MzVlNmEiLCJ1c2VybmFtZSI6Imx1Y2FzIiwiaWF0IjoxNzM5Mjc5Mjc4LCJleHAiOjE3MzkyODI4Nzh9.RWyL-dC1X_wECIqtJKpE01272fWTkkwUC_4rdpmGj0g',
+    description: 'Token do usuário',
+  })
   token: string;
+
+  @ApiProperty({ example: 3600, description: 'Tempo de expiração do token' })
   expiresIn: number;
+}
+
+export class SignInDto {
+  @ApiProperty({ example: 'lucasribeiro', description: 'Nome do usuário' })
+  username: string;
+  @ApiProperty({ example: '12345678', description: 'Senha do usuário' })
+  password: string;
 }
